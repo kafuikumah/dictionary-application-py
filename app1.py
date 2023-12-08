994d540ced6602259798d8d1d1336a802e6e0458
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-12-08 07:08:33 +0000

    Invalidate wrong inputs

--- app1.py
+++ app1.py
@@ -3,15 +3,25 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translate(word):
-    word = word.lower()
-    if word in data:
-        return data[word]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        yn = yn.upper()
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word does not exist. Please double-check it"
+        else:
+            return "Incorrect Input."
     else:
         return "The word does not exist. Please check your spelling"
 
 word = input("Enter word: ")
 
 print(translate(word))
+
+
+
